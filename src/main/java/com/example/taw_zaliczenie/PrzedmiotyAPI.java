cc6c988f2959f60f3012ad51e91c28dbff29acc6
kowalski.dariusz <kowalski.dariusz7@gmail.com>
2023-06-20 15:17:28 +0200

    v2 - dodanie generowania bazy przedmiotow przez API

--- src/main/java/com/example/taw_zaliczenie/PrzedmiotyAPI.java
+++ src/main/java/com/example/taw_zaliczenie/PrzedmiotyAPI.java
@@ -1,5 +1,7 @@
 package com.example.taw_zaliczenie;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api")
 public class PrzedmiotyAPI {
 
+    @Autowired
+    private PrzedmiotyData przedmiotyData;
 
+    @GetMapping(value = "generuj", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public void generujPrzedmioty()
+    {
+        przedmiotyData.generujPrzedmioty();
+    }
 
 }
